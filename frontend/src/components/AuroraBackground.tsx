b26c0f8fa95e70037a5d08c844d2efbbf1bba4ee
Cap Krish <eishusaivarsha@gmail.com>
2026-09-10 19:07:00 +0530

    Remove AuroraBackground component

--- frontend/src/components/AuroraBackground.tsx
+++ frontend/src/components/AuroraBackground.tsx
@@ -1,9 +1,1 @@
-export default function AuroraBackground() {
-  return (
-    <div className="aurora-bg" aria-hidden="true">
-      <div className="aurora-blob one" />
-      <div className="aurora-blob two" />
-      <div className="aurora-blob three" />
-    </div>
-  );
-}
+
